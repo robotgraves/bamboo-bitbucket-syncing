60c101722651f8a882961b513dbed051b2e7ce0b
apaul@transparent.com <apaul@transparent.com>
2017-06-14 11:51:00 -0400

    bitbucket registration is now working

--- scripts/forms.py
+++ scripts/forms.py
@@ -22,27 +22,30 @@ class IllegalStateParser(HTMLParser):
             self.results = True
 
 
-bamboo_host = "http://localhost:8085"
-bitbucket_host = "http://localhost:7990"
+bamboo_host = "http://10.0.2.15:8085"
+bitbucket_host = "http://10.0.2.15:7990"
 r = requests.session()
 
 headers = {
+    'Host': 'localhost:7990',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate',
     'DNT': '1',
     'Connection': 'keep-alive',
-    'Upgrade-Insecure-Requests': '1'
+    'Upgrade-Insecure-Requests': '1',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
 }
 
 url = str(
     'http://localhost:7990/plugins/servlet/applinks/listApplicationLinks'
 )
 
-
-response = r.get(
-    url=url
+r.get(
+    url=url,
+    headers=headers
 )
 
 url = 'http://localhost:7990/j_atl_security_check'
@@ -57,27 +60,73 @@ form = {
 
 response = r.post(
     url=url,
+    headers=headers,
     data=form
 )
 
 parsed = IllegalStateParser(data='Configure Application Links')
 parsed.feed(response.text)
 if not parsed.results:
-    print "Not Application Links Page, raising error"
+    url = str(
+        'http://localhost:7990/plugins/servlet/applinks/listApplicationLinks'
+    )
+    response = r.get(
+        url=url
+    )
+    parsed.feed(response.text)
+    if not parsed.results:
+        print response.text
+        print response.status_code
+        print "Not Application Links Page, raising error"
+        raise Exception
 
 xsrf_token = response.cookies['atl.xsrf.token']
 
-
-
-
-
-# print response.text
-# print response.status_code
+cookies = {
+    'atl.xsrf.token': xsrf_token
+}
 
 url = str(
-    "http://localhost:7990/rest/applinks/3.0/applicationlinkForm/manifest.json?url=" +
+    "http://10.0.2.15:7990/rest/applinks/3.0/applicationlinkForm/manifest.json?url=" +
     bamboo_host +
     "&_=" +
     str(int(round(time.time() * 1000))))
 
+response = r.get(
+    url=url,
+    headers=headers,
+    cookies=cookies
+)
+
+
+def parse_string_dictionary(dictionary):
+    resulting_dictionary = {}
+    items = dictionary.strip('{').strip('}').split(',')
+    for item in items:
+        split_item = item.split(":")
+        name = split_item[0].strip('"')
+        try:
+            value = split_item[1].strip('"')
+        except IndexError:
+            value = ""
+        resulting_dictionary[name] = value
+
+    return resulting_dictionary
+
+results = parse_string_dictionary(response.text)
+
+
+x = 1
+
+for name, value in results.iteritems():
+    if name == "name":
+        if value == "Atlassian+Bamboo":
+            x = 0
+            break
+
+if x == 1:
+    print response.text
+    print response.status_code
+    print "Atlassian+Bamboo was not linked"
+    raise Exception
 
