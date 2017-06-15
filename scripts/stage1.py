9312647873a44f37f2071dc77e5dc0d13ef63b77
apaul@transparent.com <apaul@transparent.com>
2017-06-15 16:10:09 -0400

    split into stages, so I can run each individually

---
+++ scripts/stage1.py
@@ -0,0 +1,168 @@
+import requests
+from requests import ConnectionError
+import json
+import time
+from HTMLParser import HTMLParser
+
+
+class IllegalStateParser(HTMLParser):
+    """
+    pulls out error codes from data sections
+    """
+    def __init__(self, data):
+        """
+        :param data: string 
+        """
+        HTMLParser.__init__(self)
+        self.data = data
+        self.results = False
+
+    def handle_data(self, data):
+        if self.data in data:
+            self.results = True
+
+
+def parse_string_dictionary(dictionary):
+    """
+    Hand over a string that should be a dictionary, and this should turn it into a dictionary
+    :param dictionary: 
+    :return: 
+    """
+    resulting_dictionary = {}
+    items = dictionary.strip('{').strip('}').split(',')
+    for item in items:
+        split_item = item.split(":")
+        name = split_item[0].strip('"')
+        try:
+            value = split_item[1].strip('"')
+        except IndexError:
+            value = ""
+        resulting_dictionary[name] = value
+
+    return resulting_dictionary
+
+
+bamboo_host = "http://10.0.2.15:8085"
+bitbucket_host = "http://10.0.2.15:7990"
+r = requests.session()
+
+headers = {
+    'Host': 'localhost:7990',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate',
+    'DNT': '1',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+}
+
+url = str(
+    'http://localhost:7990/plugins/servlet/applinks/listApplicationLinks'
+)
+
+r.get(
+    url=url,
+    headers=headers
+)
+
+url = 'http://localhost:7990/j_atl_security_check'
+
+form = {
+    'j_username': 'bamboo',
+    'j_password': 'test',
+    '_atl_remember_me': 'on',
+    'next': 'http://localhost:7990/plugins/servlet/applinks/listApplicationLinks',
+    'submit': 'Log_in'
+}
+
+response = r.post(
+    url=url,
+    headers=headers,
+    data=form
+)
+
+parsed = IllegalStateParser(data='Configure Application Links')
+parsed.feed(response.text)
+if not parsed.results:
+    url = str(
+        'http://localhost:7990/plugins/servlet/applinks/listApplicationLinks'
+    )
+    response = r.get(
+        url=url
+    )
+    parsed.feed(response.text)
+    if not parsed.results:
+        print response.text
+        print response.status_code
+        print "Not Application Links Page, raising error"
+        raise Exception
+
+xsrf_token = response.cookies['atl.xsrf.token']
+
+cookies = {
+    'atl.xsrf.token': xsrf_token
+}
+
+url = str(
+    "http://10.0.2.15:7990/rest/applinks/3.0/applicationlinkForm/manifest.json?url=" +
+    bamboo_host +
+    "&_=" +
+    str(int(round(time.time() * 1000))))
+
+response = r.get(
+    url=url,
+    headers=headers,
+    cookies=cookies
+)
+
+
+
+
+results = parse_string_dictionary(response.text)
+
+
+x = 1
+
+for name, value in results.iteritems():
+    if name == "name":
+        if value == "Atlassian+Bamboo":
+            x = 0
+            break
+
+if x == 1:
+    print response.text
+    print response.status_code
+    print "Atlassian+Bamboo was not linked"
+    raise Exception
+
+for name, value in results.iteritems():
+    if name == "id":
+        server_id = value
+
+url = "http://localhost:7990/rest/applinks/3.0/applicationlink"
+
+form = {
+    'id': server_id,
+    'name': 'Atlassian+Bamboo',
+    'rpcUrl': 'http://10.0.2.15:8085',
+    'displayUrl': 'http://192.168.253.52:8085',
+    'typeId': 'bamboo'
+}
+
+modified_headers = headers
+
+modified_headers['Accept'] = 'application/json, text/javascript, */*; q=0.01'
+modified_headers['content-Type'] = 'application/json; charset=utf-8'
+modified_headers['X-Requested-With'] = 'XMLHttpRequest'
+
+
+response = r.put(
+    url=url,
+    headers=modified_headers,
+    data=json.dumps(form),
+)
+
+print "link is complete on Bitbucket"
--- scripts/stage2.py
+++ scripts/stage2.py
@@ -59,113 +59,6 @@ headers = {
     'Cache-Control': 'no-cache',
 }
 
-# url = str(
-#     'http://localhost:7990/plugins/servlet/applinks/listApplicationLinks'
-# )
-#
-# r.get(
-#     url=url,
-#     headers=headers
-# )
-#
-# url = 'http://localhost:7990/j_atl_security_check'
-#
-# form = {
-#     'j_username': 'bamboo',
-#     'j_password': 'test',
-#     '_atl_remember_me': 'on',
-#     'next': 'http://localhost:7990/plugins/servlet/applinks/listApplicationLinks',
-#     'submit': 'Log_in'
-# }
-#
-# response = r.post(
-#     url=url,
-#     headers=headers,
-#     data=form
-# )
-#
-# parsed = IllegalStateParser(data='Configure Application Links')
-# parsed.feed(response.text)
-# if not parsed.results:
-#     url = str(
-#         'http://localhost:7990/plugins/servlet/applinks/listApplicationLinks'
-#     )
-#     response = r.get(
-#         url=url
-#     )
-#     parsed.feed(response.text)
-#     if not parsed.results:
-#         print response.text
-#         print response.status_code
-#         print "Not Application Links Page, raising error"
-#         raise Exception
-#
-# xsrf_token = response.cookies['atl.xsrf.token']
-#
-# cookies = {
-#     'atl.xsrf.token': xsrf_token
-# }
-#
-# url = str(
-#     "http://10.0.2.15:7990/rest/applinks/3.0/applicationlinkForm/manifest.json?url=" +
-#     bamboo_host +
-#     "&_=" +
-#     str(int(round(time.time() * 1000))))
-#
-# response = r.get(
-#     url=url,
-#     headers=headers,
-#     cookies=cookies
-# )
-#
-#
-#
-#
-# results = parse_string_dictionary(response.text)
-#
-#
-# x = 1
-#
-# for name, value in results.iteritems():
-#     if name == "name":
-#         if value == "Atlassian+Bamboo":
-#             x = 0
-#             break
-#
-# if x == 1:
-#     print response.text
-#     print response.status_code
-#     print "Atlassian+Bamboo was not linked"
-#     raise Exception
-#
-# for name, value in results.iteritems():
-#     if name == "id":
-#         server_id = value
-#
-# url = "http://localhost:7990/rest/applinks/3.0/applicationlink"
-#
-# form = {
-#     'id': server_id,
-#     'name': 'Atlassian+Bamboo',
-#     'rpcUrl': 'http://10.0.2.15:8085',
-#     'displayUrl': 'http://192.168.253.52:8085',
-#     'typeId': 'bamboo'
-# }
-#
-# modified_headers = headers
-#
-# modified_headers['Accept'] = 'application/json, text/javascript, */*; q=0.01'
-# modified_headers['content-Type'] = 'application/json; charset=utf-8'
-# modified_headers['X-Requested-With'] = 'XMLHttpRequest'
-#
-#
-# response = r.put(
-#     url=url,
-#     headers=modified_headers,
-#     data=json.dumps(form),
-# )
-
-print "link is complete on Bitbucket"
 
 url = 'http://localhost:8085/userlogin!doDefault.action'
 
@@ -230,7 +123,6 @@ parsed_dictionary = parse_string_dictionary(response.text)
 
 for name, value in parsed_dictionary.iteritems():
     if name == "id":
-        print "server_id = " + str(value)
         server_id = value
         break
 if not server_id:
